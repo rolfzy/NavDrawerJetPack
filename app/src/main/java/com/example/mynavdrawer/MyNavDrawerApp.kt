7d0932d99015de6b202b7faf6feda1f3cbf2cf05
Rolfzy <89122134+rolfzy@users.noreply.github.com>
2025-02-09 22:50:04 +0700

    Menambahkan NavDrawerState

--- app/src/main/java/com/example/mynavdrawer/MyNavDrawerApp.kt
+++ app/src/main/java/com/example/mynavdrawer/MyNavDrawerApp.kt
@@ -1,8 +1,6 @@
 package com.example.mynavdrawer
 
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -14,7 +12,6 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,48 +19,36 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mynavdrawer.ui.theme.MyNavDrawerTheme
-import kotlinx.coroutines.launch
 
 data class MenuItem(val title: String, val icon: ImageVector)
 
 @Composable
 fun MyNavDrawerApp() {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-    val snackbarHostState = remember { SnackbarHostState() }
-    val context = LocalContext.current
 
-    BackPressHandler(enable = drawerState.isOpen) {
-        scope.launch {
-            drawerState.close()
-        }
+    val appState = rememberMyNavDrawerState()
+
+    BackPressHandler(enable = appState.drawerState.isOpen) {
+        appState.onBackPress()
 
     }
-
 
 
     val items = listOf(
@@ -85,25 +70,17 @@ fun MyNavDrawerApp() {
 
 
     Scaffold(
-        snackbarHost = { SnackbarHost(snackbarHostState) },
+        snackbarHost = { SnackbarHost(appState.snackbarHostState) },
         topBar = {
             MyTopBar(
-                onMenuClick = {
-                    scope.launch {
-                        if (drawerState.isClosed) {
-                            drawerState.open()
-                        } else {
-                            drawerState.close()
-
-                        }
-                    }
-
-                }
+                onMenuClick = appState::onMenuClick
             )
-        }
+        },
     ) { paddingValues ->
         ModalNavigationDrawer(modifier = Modifier.padding(paddingValues),
-            drawerState = drawerState, gesturesEnabled = drawerState.isOpen, drawerContent =
+            drawerState = appState.drawerState,
+            gesturesEnabled = appState.drawerState.isOpen,
+            drawerContent =
 
             {
                 ModalDrawerSheet {
@@ -114,29 +91,14 @@ fun MyNavDrawerApp() {
                             label = { Text(item.title) },
                             selected = item == selectedItem.value,
                             onClick = {
-                                scope.launch {
-                                    drawerState.close()
-                                    val snackbarResult = snackbarHostState.showSnackbar(
-                                        message = context.resources.getString(
-                                            R.string.coming_soon,
-                                            item.title
-                                        ),
-                                        actionLabel = context.resources.getString(R.string.subscribe_question),
-                                        withDismissAction = true,
-                                        duration = SnackbarDuration.Short
-                                    )
-                                    if (snackbarResult == SnackbarResult.ActionPerformed) {
-                                        Toast.makeText(
-                                            context,
-                                            context.resources.getString(R.string.subscribed_info),
-                                            Toast.LENGTH_LONG
-                                        ).show()
-
-                                    }
-                                }
+                                appState.onItemSelected(item)
+                                selectedItem.value = item
                             },
+
                             modifier = Modifier.padding(horizontal = 12.dp)
+
                         )
+
                     }
                 }
             },
@@ -148,7 +110,7 @@ fun MyNavDrawerApp() {
                     contentAlignment = Alignment.Center
                 ) {
                     Text(
-                        if (drawerState.isClosed) {
+                        if (appState.drawerState.isClosed) {
                             stringResource(R.string.swipe_to_open)
                         } else {
                             stringResource(R.string.swipe_to_close)
@@ -163,11 +125,12 @@ fun MyNavDrawerApp() {
     }
 
 }
+
 @Composable
-fun BackPressHandler(enable: Boolean = true , onBackPressed: () -> Unit){
+fun BackPressHandler(enable: Boolean = true, onBackPressed: () -> Unit) {
     val currentOnBackPressed by rememberUpdatedState(onBackPressed)
     val backCallback = remember {
-        object : OnBackPressedCallback(enable){
+        object : OnBackPressedCallback(enable) {
             override fun handleOnBackPressed() {
                 currentOnBackPressed()
             }
@@ -176,17 +139,17 @@ fun BackPressHandler(enable: Boolean = true , onBackPressed: () -> Unit){
     SideEffect {
         backCallback.isEnabled = enable
     }
-    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current){
+    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current) {
         " No  onBackPressedDispatcherOwner was Provided via LocalOnBackPressedDispatcherOwner"
     }.onBackPressedDispatcher
 
     val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(lifecycleOwner,backDispatcher) {
+    DisposableEffect(lifecycleOwner, backDispatcher) {
         backDispatcher.addCallback(lifecycleOwner, backCallback)
         onDispose {
             backCallback.remove()
         }
-        
+
     }
 }
 
